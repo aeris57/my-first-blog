a0bb559df36e953a991d60e5022c242cf8e0dbc1
Vicky <vix.hodges@gmail.com>
2016-09-28 16:16:50 +0100

    Removed the approval button so comments show up

--- blog/views.py
+++ blog/views.py
@@ -79,13 +79,6 @@ def add_comment_to_post(request, pk):
 
 
 @login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
-
-
-@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
